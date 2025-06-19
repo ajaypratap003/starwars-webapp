0baee884f78d978ed7a2aef18d29a341d387619a
ajaypratap003 <ajay.pratap233@gmail.com>
2025-06-19 18:06:42 +0530

    remove unused  React imports

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectedFavourites } from "../store/selectors";
 import { deleteAllFavourites, deleteFavouriteByUID } from "../store/starwarsSlice";
